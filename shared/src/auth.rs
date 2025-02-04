3014d6f2bc32956b3b6049dc2a07cb45fa68410a
qthree <qthree3@gmail.com>
2025-02-05 02:02:49 +0700

    Implement AuthMethod::HmacSha256
    ApiRequester successfully GETs "/market" in test

---
+++ shared/src/auth.rs
@@ -0,0 +1,74 @@
+use std::borrow::Cow;
+
+use anyhow::{bail, Context as _};
+use reqwest::Url;
+
+use super::AuthMethod;
+
+/// return UNIX timestamp in milliseconds since 'Thu Jan 01 1970 00:00:00.000'
+/// u64 is enough for another ~585 million years
+fn timestamp() -> u64 {
+    std::time::UNIX_EPOCH.elapsed().map_or(0, |ts| ts.as_millis().try_into().unwrap_or(u64::MAX))
+}
+
+// TODO: optimize by making query sorted in the first place, in send_request?
+// But other auth methods maybe don't want this
+// Also will cause request query to be sorted
+fn timestamped_sorted_params(url: &Url, timestamp: &str) -> String {
+    let mut query: Vec<_> = url.query_pairs().collect();
+    query.push((Cow::Borrowed("signTimestamp"), Cow::Borrowed(timestamp)));
+    query.sort_unstable();
+    let mut ser = form_urlencoded::Serializer::for_suffix(String::new(), 0);
+    ser.extend_pairs(query);
+    ser.finish()
+}
+
+mod hmac_sha256 {
+    use base64::prelude::{Engine as _, BASE64_STANDARD};
+    use hmac::digest::Mac as _;
+
+    type HmacSha256 = hmac::Hmac<sha2::Sha256>;
+
+    // TODO: reuse buffers?
+    pub(super) fn sign_payload(payload: &str, secret_key: &str) -> String {
+        // call hash function
+        let mut mac = HmacSha256::new_from_slice(secret_key.as_bytes()).expect("HMAC can take key of any size");
+        mac.update(payload.as_bytes());
+        let mac_output = mac.finalize();
+    
+        // encode hash with base64
+        BASE64_STANDARD.encode(mac_output.into_bytes())
+    }
+}
+
+impl AuthMethod {
+    pub(super) fn apply(&self, req: &mut reqwest::Request) -> anyhow::Result<()> {
+        match self {
+            AuthMethod::HmacSha256 { api_key, secret_key } => {
+                let method = req.method().as_str();
+
+                // TODO: implement POST, PUT, DELETE
+                if method != "GET" {
+                    bail!("only GET method implemented for HmacSha256 authentication mode yet");
+                }
+                
+                // url without authority, domain and query
+                let path = req.url().path();
+
+                let timestamp = timestamp().to_string();
+
+                let params = timestamped_sorted_params(req.url(), &timestamp);
+
+                let payload = format!("{method}\n{path}\n{params}");
+
+                let sign = hmac_sha256::sign_payload(&payload, secret_key);
+
+                let headers = req.headers_mut();
+                headers.append("key", api_key.try_into().context("header value from public key")?);
+                headers.append("signature", sign.try_into().context("header value from signature")?);
+                headers.append("signTimestamp", timestamp.try_into().context("header value from timestamp")?);
+            },
+        }
+        Ok(())
+    }
+}
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,23 +1,76 @@
-use anyhow::Context;
-use reqwest::{IntoUrl, Url};
+use std::{borrow::Cow, sync::Arc, time::Duration};
+
+use anyhow::{bail, Context};
+use reqwest::{header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE}, IntoUrl, RequestBuilder, Url};
 use serde::de::DeserializeOwned;
 
-pub struct ApiRequester {
-    // it's recommended to build reqwest::Client once, not per each request
+mod auth;
+
+#[derive(Debug)]
+pub struct ApiFactory {
+    // it's recommended to build reqwest::Client once, not per each request, has to do with TLS initialization and connections pool
+    // but can we use same reqwest client for different end-users of the service? Should be ok if we don't use reqwest::ClientBuilder::cookie_store or cookie_provider
+    // this allows as to use only one reqwest client per app
     client: reqwest::Client,
-    base_url: Url,
+}
+
+impl Default for ApiFactory {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug)]
+pub struct ApiConfig {
+    pub base_url: Url,
+    pub auth: AuthMethod,
+}
+
+#[derive(Debug)]
+pub enum AuthMethod {
+    HmacSha256{
+        api_key: String,
+        secret_key: String,
+    }
+}
+
+impl ApiFactory {
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::Client::new(),
+        }
+    }
+    pub fn make_requester(&self, config: impl Into<Arc<ApiConfig>>) -> ApiRequester {
+        //let headers = Default::default();
+        ApiRequester {
+            config: config.into(),
+            client: self.client.clone(),
+            //headers,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ApiRequester {
+    config: Arc<ApiConfig>,
+    client: reqwest::Client,
+    //headers: HeaderMap,
 }
 
 impl ApiRequester {
-    pub fn new(base_url: impl IntoUrl) -> anyhow::Result<Self> {
-        Ok(Self {
-            base_url: base_url.into_url().context("parse base url")?,
-            client: reqwest::Client::new(),
-        })
+    async fn send_request(&self, method: reqwest::Method, path: &str) -> anyhow::Result<reqwest::Response> {
+        let url = self.config.base_url.join(path).context("join base url and path")?;
+
+        let mut req = self.client.request(method, url)
+            .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+            .build()
+            .context("build request")?;
+        self.config.auth.apply(&mut req).context("apply authentication to request")?;
+        self.client.execute(req).await.context("execute request")
     }
-    pub async fn get_string(&self, path: &str) -> anyhow::Result<String> {
-        let url = self.base_url.join(path).context("join base url and path")?;
-        let res = self.client.get(url).send().await.context("send GET request")?;
+    async fn get_string(&self, path: &str) -> anyhow::Result<String> {
+        let res = self.send_request(reqwest::Method::GET, path).await?;
+        // we don't use Response::json to separate different kinds of errors
         res.text().await.context("receive JSON response")
     }
     pub async fn get_json<T: DeserializeOwned>(&self, path: &str) -> anyhow::Result<T> {
@@ -28,10 +81,14 @@ impl ApiRequester {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[tokio::test]
     async fn test_api_requester() {
-        let requester = super::ApiRequester::new("https://api.poloniex.com").unwrap();
-        let markets = requester.get_string("markets").await.unwrap();
-        dbg!(markets);
+        let api_key = std::env::var("API_KEY").expect("API_KEY env var");
+        let secret_key = std::env::var("SECRET_KEY").expect("SECRET_KEY env var");
+        let requester = ApiFactory::new().make_requester(ApiConfig{base_url: "https://api.poloniex.com".try_into().unwrap(), auth: AuthMethod::HmacSha256 { api_key, secret_key }});
+        let markets: serde_json::Value = requester.get_json("markets").await.unwrap();
+        println!("{}", serde_json::to_string_pretty(&markets).unwrap());
     }
 }
